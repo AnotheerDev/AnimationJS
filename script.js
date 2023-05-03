e45d07a4d46a8f62d86b4c1353764f457148f650
AnotheerDev <leger.julien94@gmail.com>
2023-05-03 16:55:03 +0200

    1st commit js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const icone = document.querySelector("#iconeFacebook");
+
+iconeFacebook.addEventListener("click", function() {
+    iconeFacebook.classList.toggle("animation");
+});
+
+const icone2 = document.querySelector("#iconeTwitter");
+
+iconeTwitter.addEventListener("click", function() {
+    iconeTwitter.classList.toggle("animation");
+});
+
+const icone3 = document.querySelector("#iconeInstagram");
+
+iconeInstagram.addEventListener("click", function() {
+    iconeInstagram.classList.toggle("animation");
+});
+
 
